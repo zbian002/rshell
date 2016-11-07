899fe1f9474d5a570262241a668f56b1a4813262
zbian002 <zbian002@ucr.edu>
2016-11-07 02:08:57 -0800

    Update rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,1 +1,95 @@
+#include "rshell.h"
 
+using namespace std;
+
+void type_prompt()
+{
+    struct passwd *pwd;
+    char hostname[max_length];
+    int length;
+    pwd = getpwuid(getuid());
+    if(gethostname(hostname,max_length)==0)
+	cout << "[rshell]" << pwd->pw_name << "@" << hostname << "$ ";
+    else
+	cout << "[rshell]" << pwd->pw_name << "@" << "anonymous" << "$ ";
+    return;
+}
+
+char **parse_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = (char**)malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(1);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = (char**)realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(1);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+int command_exec(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+  pid = fork();
+  switch(pid){  
+   case -1:  
+     perror("fork failed");  
+     exit(1);  
+   case 0:  
+     execvp(args[0], args);
+     perror("execvp failed");  
+     exit(1);  
+   default:  
+     do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));  
+  }
+  return 1;
+}  
+
+void loop(void)
+{
+  char **args;
+  int status;
+  char *line = NULL;
+  string input;
+
+  do {
+    type_prompt();
+    getline(cin,input);
+    if (input=="exit")
+	break;
+    char*line = (char*)input.data();
+    args = parse_line(line);
+    status = command_exec(args);
+    free(args);
+  }while (status);
+}
+
+int main(int argc, char **argv)
+{
+ 
+  loop();
+
+  return 0;
+}
