6e4b0640d0b84de03f76591e036e192427698c85
zbian002 <zbian002@ucr.edu>
2016-11-07 04:12:22 -0800

    Update rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,4 +1,16 @@
-#include "rshell.h"
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <pwd.h>
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+const int max_length = 256;
 
 using namespace std;
 
