7403dee7a9ced61bcff5132eeb5e9a12099fa17d
zbian002 <zbian002@ucr.edu>
2016-11-07 02:09:51 -0800

    Create rshell.h

---
+++ src/rshell.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <pwd.h>
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+const int max_length = 256;
